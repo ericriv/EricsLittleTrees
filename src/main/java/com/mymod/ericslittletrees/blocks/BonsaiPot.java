27e9ffc62fc4a1e39b7fb0f2f8d8471c9e6f21aa
ericriv88 <ericriv88@gmail.com>
2024-08-06 12:39:21 -0700

    Bonsai pot now works as normal flower pot

--- src/main/java/com/mymod/ericslittletrees/blocks/BonsaiPot.java
+++ src/main/java/com/mymod/ericslittletrees/blocks/BonsaiPot.java
@@ -2,10 +2,11 @@ package com.mymod.ericslittletrees.blocks;
 
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.Blocks;
 
 public class BonsaiPot extends FlowerPotBlock {
 
     public BonsaiPot(BlockBehaviour.Properties properties) {
-        super(null, null, properties);
+        super(() -> (FlowerPotBlock) Blocks.FLOWER_POT, () -> Blocks.AIR, properties);
     }
 }
